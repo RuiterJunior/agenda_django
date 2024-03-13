f5e2a1d2035c79dc4f896aa61682dc50d9368f96
Ruiter José <ruiter.wow@hotmail.com>
2024-03-12 22:44:25 -0300

    Exibindo os contatos no site e a criação de links para visualização dos links individualmente.

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact'),
+    # path('<contact_first_name>/', views.contact, name='contact'),
 ]
--- contact/views/views_contact.py
+++ contact/views/views_contact.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from contact import models
 
 # Create your views here.
 def index(request):
-    return render(request, 'contact/index.html')
+    contacts = models.Contact.objects.filter(show=True).order_by('-id')[:10]
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(request, 'contact/index.html', context)
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(models.Contact.objects.filter(pk=contact_id, show=True))
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title,
+    }
+
+    return render(request, 'contact/contact.html', context)
