34c955a729aeb191de810a894e734956774de3fd
Ruiter José <ruiter.wow@hotmail.com>
2024-03-21 22:34:17 -0300

    Melhorando o form de cadastro e incrementando o csrf token.

--- contact/views/views_forms.py
+++ contact/views/views_forms.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render,get_object_or_404,redirect
-from django.db.models import Q
-from contact import models
-from django.core.paginator import Paginator
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 # Create your views here.
 def create(request):
-    context = {
-        
-    }
+    if request.method == 'POST':
+        context = {'form': ContactForm(request.POST)}
+
+        return render(request, 'contact/create.html', context)
+
+    context = {'form': ContactForm()}
 
     return render(request, 'contact/create.html', context)
 
