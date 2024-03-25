72ea68062579647cb2cd1dadcd2f7bc306689bb4
Ruiter José <ruiter.wow@hotmail.com>
2024-03-24 22:53:56 -0300

    Melhorias nos formulários.

--- contact/forms.py
+++ contact/forms.py
@@ -3,15 +3,36 @@ from django import forms
 from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
+    
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     class Meta:
         model = Contact
-        fields =('first_name', 'last_name', 'phone')
+        fields =('first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture',)
 
     def clean(self):
-        # cleaned_data = self.changed_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        # self.add_error('first_name', ValidationError('Mensagem de erro', code='invalid'))
+        if first_name == last_name:
+            msg = ValidationError('Primeiro nome não pode ser igual ao segundo', code='invalid')
 
-        # self.add_error('first_name', ValidationError('Mensagem de erro 2', code='invalid'))
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error('first_name', ValidationError('Nome inválido', code='invalid'))
+
+        return first_name
--- contact/urls.py
+++ contact/urls.py
@@ -6,6 +6,8 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
